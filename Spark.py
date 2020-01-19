44c9a57c104a590a208f0039d3affd187d956c56
YMO96 <47666267+YMO96@users.noreply.github.com>
2020-01-19 18:44:21 +0100

    Traitement de json dans spark (Calcul de la vitesss et but)

--- Spark.py
+++ Spark.py
@@ -1,4 +1,4 @@
-from pyspark.sql.functions import monotonically_increasing_id, col, lag, trim, substring, when, udf,sqrt, pow, round
+from pyspark.sql.functions import monotonically_increasing_id, col, lag, trim, substring, when, udf,sqrt, pow, round,sum
 from pyspark.sql.types import StringType
 from pyspark.sql.window import Window
 		
@@ -12,11 +12,14 @@ df = df.withColumn( "x" , when(substring(trim(df.state),1,10).contains('n')==Fal
 df = df.withColumn( "y" , when(substring(trim(df.state),1,10).contains('n')==False,df["position"].getItem(1)).otherwise(0))
 df = df.withColumn("prev_x",lag(df.x).over(my_window))
 df = df.withColumn("prev_y",lag(df.y).over(my_window))
-df = df.withColumn("goal",when((substring(trim(df.prev_state),1,10).contains('n')==False) & (substring(trim(df.state),1,10).contains('n')) & ( df.prev_y>50) & (df.prev_y<400)& ((df.prev_x < 100) | (df.prev_x >400)) , "1").otherwise("0"))
+df = df.withColumn("goal",when((substring(trim(df.prev_state),1,10).contains('n')==False) & (substring(trim(df.state),1,10).contains('n')) & ( df.prev_y>150) & (df.prev_y<300)& ((df.prev_x < 100) | (df.prev_x >500)) , 1).otherwise(0))
 df = df.withColumn("goal",lag(df.goal).over(my_window))
+df.groupBy('goal').count().show()
 df = df.filter(df.goal.isNotNull())
+
 df = df.withColumn("vitesse",sqrt(pow(df.x-df.prev_x,2)+pow(df.y-df.prev_y,2)))
 df = df.withColumn("vitesse",round(df["vitesse"]*30/500,2))
+df = df.withColumn("total_goal",sum(df.goal).over(my_window))
 df.show()
 df.write.json("file:///home/ymo/babyfoot.json",mode = 'overwrite')
 exit()
