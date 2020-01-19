31cfff04ff15b3e49f13749efb279598e244d188
YMO96 <47666267+YMO96@users.noreply.github.com>
2020-01-19 19:05:05 +0100

    Traitement de json dans spark (Calcul de la vitesss et but)

--- Spark.py
+++ Spark.py
@@ -1,4 +1,4 @@
-from pyspark.sql.functions import monotonically_increasing_id, col, lag, trim, substring, when, udf,sqrt, pow, round,sum
+from pyspark.sql.functions import monotonically_increasing_id, col, lag, trim, substring, when, udf,sqrt, pow, round,sum, lead
 from pyspark.sql.types import StringType
 from pyspark.sql.window import Window
 		
@@ -7,17 +7,17 @@ df = df.withColumn("id", monotonically_increasing_id())
 my_window = Window.partitionBy().orderBy("id")
 df = df.withColumnRenamed( "Ball position" , "position" )
 df = df.withColumnRenamed( "Ball state" , "state" )
-df = df.withColumn("prev_state",lag(df.state).over(my_window))
+df = df.withColumn("nxt_state",lead(df.state).over(my_window))
 df = df.withColumn( "x" , when(substring(trim(df.state),1,10).contains('n')==False,df["position"].getItem(0)).otherwise(0))
 df = df.withColumn( "y" , when(substring(trim(df.state),1,10).contains('n')==False,df["position"].getItem(1)).otherwise(0))
-df = df.withColumn("prev_x",lag(df.x).over(my_window))
-df = df.withColumn("prev_y",lag(df.y).over(my_window))
-df = df.withColumn("goal",when((substring(trim(df.prev_state),1,10).contains('n')==False) & (substring(trim(df.state),1,10).contains('n')) & ( df.prev_y>150) & (df.prev_y<300)& ((df.prev_x < 100) | (df.prev_x >500)) , 1).otherwise(0))
-df = df.withColumn("goal",lag(df.goal).over(my_window))
+df = df.withColumn("nxt_x",lead(df.x).over(my_window))
+df = df.withColumn("nxt_y",lead(df.y).over(my_window))
+df = df.withColumn("goal",when((substring(trim(df.state),1,10).contains('n')==False) & (substring(trim(df.nxt_state),1,10).contains('n')) & ( df.y>150) & (df.y<300)& ((df.x < 100) | (df.x >500)) , 1).otherwise(0))
+df = df.withColumn("goal",lead(df.goal).over(my_window))
 df.groupBy('goal').count().show()
 df = df.filter(df.goal.isNotNull())
 
-df = df.withColumn("vitesse",sqrt(pow(df.x-df.prev_x,2)+pow(df.y-df.prev_y,2)))
+df = df.withColumn("vitesse",sqrt(pow(df.x-df.nxt_x,2)+pow(df.y-df.nxt_y,2)))
 df = df.withColumn("vitesse",round(df["vitesse"]*30/500,2))
 df = df.withColumn("total_goal",sum(df.goal).over(my_window))
 df.show()
